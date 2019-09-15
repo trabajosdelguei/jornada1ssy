1115e6919fe98e77d51546294d77074666f19cd1
Huruz <huruz_wwe@hotmail.com>
2019-09-15 17:02:40 -0500

    Get de registrados

--- php/conexionBD.php
+++ php/conexionBD.php
@@ -7,19 +7,4 @@ $basededatos= "jornadassy";
 $conexion= mysqli_connect($servidor,$usuario,$contraseña) or die("No se ha podido conectar a la BD del Servidor");
 
 $db= mysqli_select_db($conexion,$basededatos) or die("No se ha podido conectar a ".$basededatos);
-
-	
-$consulta = "SELECT * FROM inscritos";
-
-	
-$resultado = mysqli_query( $conexion, $consulta ) or die("Consulta fallida");
-
-
-
-while ($columna = mysqli_fetch_array( $resultado ))
-{
- echo $columna['nombre'] . " " . $columna['email'] .  " " . $columna['institucion'],  " " . $columna['confimacion'] .  " " . $columna['asistencia'];
-}
-
-mysqli_close( $conexion );
 ?>
